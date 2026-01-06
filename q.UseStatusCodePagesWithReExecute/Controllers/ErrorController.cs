5dbc2ec544a18e510bc6b1c6ea75160ee177a2b1
KOKUMUbooker <bookerochieng20@gmail.com>
2026-01-06 08:44:21 +0300

    Complete UseStatusCodePagesWithReExecute demo

---
+++ q.UseStatusCodePagesWithReExecute/Controllers/ErrorController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace q.UseStatusCodePagesWithReExecute.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // This route catches any error code passed in the URL, e.g., /Error/404
+        [Route("Error/{statusCode}")]
+        public IActionResult HttpStatusCodeHandler([FromRoute] int statusCode)
+        {
+            // Set a proper error message based on the status code
+            switch (statusCode)
+            {
+                case 404:
+                    ViewBag.ErrorMessage = "Sorry, the resource you requested could not be found.";
+                    break;
+                default:
+                    ViewBag.ErrorMessage = "An unexpected error occurred. Please try again later.";
+                    break;
+            }
+            // Render NotFound view
+            return View("NotFound");
+        }
+    }
+}
