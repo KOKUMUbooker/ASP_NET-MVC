f1bd61960b85e57c92bc057f092fa4ba3c9c6db8
KOKUMUbooker <bookerochieng20@gmail.com>
2026-01-06 06:58:20 +0300

    Remove if check for dev environment

---
+++ q.UseStatusCodePagesWithRedirectsDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace q.UseStatusCodePagesWithRedirectsDemo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
