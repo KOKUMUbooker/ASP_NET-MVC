f270d0f751d7c6dba47d8031d090c2cc367722c6
KOKUMUbooker <bookerochieng20@gmail.com>
2026-01-08 05:52:29 +0300

    Complete CSRF handling demo

---
+++ q.CSRFHandlingDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace q.CSRFHandlingDemo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
