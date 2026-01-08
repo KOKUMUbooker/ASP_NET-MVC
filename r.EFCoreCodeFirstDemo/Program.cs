948a847d4a868fc08090c2ac8b47d4bee9353221
KOKUMUbooker <bookerochieng20@gmail.com>
2026-01-08 11:27:36 +0300

    Complete EF Core install demo

---
+++ r.EFCoreCodeFirstDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace r.EFCoreCodeFirstDemo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Packages to install : 
+        // 1. Microsoft.EntityFrameworkCore.SqlServer // or a different DB provider
+        //    dotnet add package Microsoft.EntityFrameworkCore.SqlServer --version 8.*
+        // 2. Microsoft.EntityFrameworkCore.Tools
+        //    dotnet add package Microsoft.EntityFrameworkCore.Tools --version 8.*
+        
+        Console.WriteLine("Hello, World!");
+    }
+}
