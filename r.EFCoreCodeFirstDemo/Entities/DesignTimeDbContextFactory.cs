815057c3d7b50ad977500ad4414e93df9a9f5621
KOKUMUbooker <bookerochieng20@gmail.com>
2026-01-09 20:13:44 +0300

    Complete DBString configuration demo

---
+++ r.EFCoreCodeFirstDemo/Entities/DesignTimeDbContextFactory.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace r.EFCoreCodeFirstDemo.Entities
+{
+    public class EFCoreDbContextFactory
+        : IDesignTimeDbContextFactory<EFCoreDbContext>
+    {
+        public EFCoreDbContext CreateDbContext(string[] args)
+        {  
+            // 1. Read connection string from appsettings.json file 
+            var configBuilder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json") // Specify the configuration file to load.
+                .Build(); // Build the configuration object, making it ready to retrieve values.
+            var configSection = configBuilder.GetSection("ConnectionStrings");
+            var connectionString = configSection["SQLServerConnection"] ?? null;
+
+            // 2. Use read connection string to connect to SQL database
+            var optionsBuilder = new DbContextOptionsBuilder<EFCoreDbContext>();
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new EFCoreDbContext(optionsBuilder.Options);
+        }
+    }
+}
